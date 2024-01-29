7fa9a914499c96cc35f6e90318d51a4ff106961d
haouch_m <malikhaouche@40-ec-99-f1-2b-c4>
2024-01-29 14:20:14 +0100

    rename file

---
+++ open_closed/solution.py
@@ -0,0 +1,15 @@
+def open_closed(s: str) -> bool:
+    stock = []
+    wordbook = {'(': ')', '[': ']', '{': '}', "'": "'", '"': '"'}
+
+    for i in s:
+        if i in wordbook.keys():
+            stock.append(i)
+        elif i in wordbook.values():
+            if not stock:
+                return False
+            end_str = stock.pop()
+            if wordbook[end_str] != i:
+                return False
+
+    return len(stock) == 0
