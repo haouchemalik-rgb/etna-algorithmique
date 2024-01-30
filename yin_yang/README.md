98f1017b6449c36eb9e03129c899fb85a83a0bc9
Clément Schobert <cleme77@hotmail.fr>
2024-01-30 11:13:12 +0100

    [ADD] - exo 11: ying_yang

---
+++ yin_yang/README.md
@@ -0,0 +1,6 @@
+# Groupe de schobe_c 1026222
+
+Réutilisation de la solution de l'exercice 2: open_closed
+c'est le meme principe sauf que les parenthèses doivent être imbriqués correctement
+
+a la fin on check si len(stock) est pair pour le cas des guillemets (pour les parenthèses len(stock) doit être égale à 0 car on retire les char du tableau au fur et à mesure)
---
+++ yin_yang/solution.py
@@ -0,0 +1,15 @@
+def yin_yang(s: str) -> bool:
+    stock = []
+    wordbook = {'(': ')', '[': ']', '{': '}', "'": "'", '"': '"'}
+
+    for i in s:
+        if i in wordbook.keys():
+            stock.append(i)
+        elif i in wordbook.values():
+            if not stock:
+                return False
+            end_str = stock.pop()
+            if wordbook[end_str] != i:
+                return False
+
+    return len(stock) % 2 == 0
