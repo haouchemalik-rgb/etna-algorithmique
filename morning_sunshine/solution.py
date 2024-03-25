afb8d98b489da4c7a98815fdd098f7d954b51b96
haouch_m <malikhaouche@40-ec-99-f1-2b-c4>
2024-03-25 10:44:43 +0100

    morning_sunshine/

--- morning_sunshine/solution.py
+++ morning_sunshine/solution.py
@@ -2,9 +2,10 @@ def morning_sunshine(numbers):
     if not numbers:
         return []
 
-    max_num = 0
+    max_num = float('-inf')
     result = []
 
+    # Parcours la liste à l'envers
     for i in range(len(numbers) - 1, -1, -1):
         if numbers[i] > max_num:
             result.append(numbers[i])
